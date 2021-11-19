ff9fe82b3b722fce12c55793215ac51d88a30fe7
Chev <chev@chevtek.io>
2021-11-19 12:00:27 -0700

    Update game countdowns.

--- functions/cron-jobs.ts
+++ functions/cron-jobs.ts
@@ -1,5 +1,6 @@
 import moment from "moment-timezone";
 import gameCountdown from "./game-countdown";
+import haloIsHere from "./halo-is-here";
 import onThisDay from "./on-this-day";
 import sloganChecker from "./slogan-checker";
 // import eventReminders from "./event-reminders";
@@ -24,6 +25,11 @@ export default async function () {
           gameCountdown()
         ]);
       }
+
+      if (moment.duration(now.diff(moment(new Date("12/8/2021 11:00:00 AM")))).asMinutes() === 0) {
+        await haloIsHere();
+      }
+
 
     } catch (err) {
       console.log(err);
--- functions/halo-is-here.ts
+++ functions/halo-is-here.ts
@@ -1,6 +1,5 @@
 import discordClient from "../discord-client";
 import { MessageEmbed, TextChannel } from "discord.js";
-import moment from "moment";
 import config from "../config";
 
 const {
@@ -10,18 +9,13 @@ const {
 export default async function () {
   const haloChannel = (discordClient.channels.cache.get(HALO_CHANNEL_ID)
     ?? await discordClient.channels.fetch(HALO_CHANNEL_ID)) as TextChannel;
-  const now = moment();
-  const haloDate = moment(new Date("12/8/2021 11:00:00 AM"));
-  if (now > haloDate) return;
-  const haloDaysRemaining = Math.round(moment.duration(haloDate.diff(now)).asDays());
-  const timeUntil = now.to(haloDate);
-  const haloImage = `${haloDaysRemaining.toString().padStart(2, "0")}.jpg`;
+  const haloImage = `00.jpg`;
   const haloMessage = new MessageEmbed()
-    .setTitle(`Halo Infinite campaign ${timeUntil}!`)
+    .setTitle(`HALO INFINITE CAMPAIGN IS LIVE!`)
     .setColor("#5F7550")
     .attachFiles(["./images/chevtek.png", `./images/halo-infinite/${haloImage}`])
     .setImage(`attachment://${haloImage}`)
-    .setDescription(`December 8th, 2021 at 10am PST`)
+    .setDescription(`PARTY TIME!`)
     .setFooter("Powered by Chevtek", "attachment://chevtek.png");
 
   await Promise.all([
